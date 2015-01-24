991d0775350b336ed1eaa6ecb7186aca060fdfa3
Adam Le Doux <aledoux6@gatech.edu>
2015-01-23 20:25:38 -0500

    bunch a generated files

---
+++ GGJ_2015_Unity/Assets/Assets/Scripts/Combat/Avatar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Avatar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ControllerInput.Button(1, Button.Xbox_A)) {
+			print ("test!");
+		}
+	}
+}
