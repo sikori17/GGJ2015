e1e66ede2c1eb257ae0ac1349b257ca13beae6d0
Adam Le Doux <aledoux6@gatech.edu>
2015-01-24 15:18:23 -0500

    start screen

---
+++ GGJ_2015_Unity/Assets/Assets/Scripts/Polish/TextFlash.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextFlash : MonoBehaviour {
+
+	public Color[] colors;
+	public ARLTimer cycleTimer;
+	int index;
+
+	// Use this for initialization
+	void Start () {
+		index = 0;
+		GetComponent<TextMesh>().color = colors[index];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (cycleTimer.IsDone()) {
+			index = (index + 1) % colors.Length;
+			GetComponent<TextMesh>().color = colors[index];
+			cycleTimer.Restart();
+		}
+	}
+}
