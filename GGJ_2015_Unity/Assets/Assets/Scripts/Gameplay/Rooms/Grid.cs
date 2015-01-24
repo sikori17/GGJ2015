d2a5073583b47f7aead8755bb3cd178a42e0d4be
Kyle Blevens <kblevens3@gmail.com>
2015-01-23 20:25:25 -0500

    Starting rooms (trying to fix input asset file)

---
+++ GGJ_2015_Unity/Assets/Assets/Scripts/Gameplay/Rooms/Grid.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	public Room[][] Rooms;
+
+	public GameObject topLeftCorner;
+	public GameObject topRightCorner;
+	public GameObject bottomLeftCorner;
+	public GameObject bottomRightCorner;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GGJ_2015_Unity/Assets/Assets/Scripts/Gameplay/Rooms/Room.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room : MonoBehaviour {
+
+	public Wall northWall;
+	public Wall southWall;
+	public Wall eastWall;
+	public Wall westWall;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
