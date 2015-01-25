ddf445c15b5b38ad88d0f259d11dae46beca49f5
Adam Le Doux <aledoux6@gatech.edu>
2015-01-25 16:33:57 -0500

    added game over scenes

---
+++ GGJ_2015_Unity/Assets/NotAssets/Scripts/Polish/AnimateAvatar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateAvatar : MonoBehaviour {
+
+	public ARLTimer animateTimer;
+	public float moveSpeed;
+	public Vector3 moveDir;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Avatar>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (animateTimer.IsDone()) {
+			GetComponent<Avatar>().enabled = true;
+		}
+		else {
+			transform.Translate(moveSpeed * moveDir * Time.deltaTime);
+		}
+	}
+}
